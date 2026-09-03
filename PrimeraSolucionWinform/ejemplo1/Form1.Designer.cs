20be5aba0ccb476d447a7e7a2b92c79c4de31a2b
Dana Milena Erro <danamerro@gmail.com>
2026-09-02 21:22:04 -0300

    primeros pasos winform

---
+++ PrimeraSolucionWinform/ejemplo1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ejemplo1
+{
+    //ACA SE CENTRALIZA TODO EL DISEÑO CENTRAL (SE AUTOGENERA EL CODIGO DE LAS COSAS QUE ARRASTRAMOS EN FORM1.CS(Diseño))
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(257, 135);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(97, 55);
+            this.btnSaludar.TabIndex = 0;
+            this.btnSaludar.Text = "SALUDAR";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 387);
+            this.Controls.Add(this.btnSaludar);
+            this.Name = "Form1";
+            this.Text = "PRIMERA APP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+    }
+}
+
