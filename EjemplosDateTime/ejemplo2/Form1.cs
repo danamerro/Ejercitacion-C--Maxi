ebfb368edc04482b55082cd6dd752c772565b5b5
Dana Milena Erro <danamerro@gmail.com>
2026-09-08 16:03:50 -0300

    Tipo DateTime

---
+++ EjemplosDateTime/ejemplo2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba1_Click(object sender, EventArgs e)
+        {
+            //devuelve la fecha marcada 
+            //MessageBox.Show("La fecha seleccionada es: " + dtpFecha.Value);
+            //devuelve en formato dia mes y año
+            //MessageBox.Show("La fecha seleccionada es: " + dtpFecha.Value.ToString("dd/MM/yyyy"));
+            //se usa para capturar una fecha y guardarla 
+            DateTime fecha1;
+            fecha1 = dtpFecha.Value;
+            MessageBox.Show("La fecha seleccionada es: " + fecha1.ToString("dd/MM/yyyy"));
+        }
+
+        private void btnPrueba2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La fecha seleccionada es: " + calFecha.SelectionStart.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ ejemplo1/ejemplo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = DateTime.Now;
+
+            //Console.WriteLine("La fecha es: " + fecha.AddDays(5));
+            Console.WriteLine("La fecha es: " + fecha.ToShortDateString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
