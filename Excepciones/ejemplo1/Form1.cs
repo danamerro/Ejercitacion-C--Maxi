43613b754b0e4d16bb07e85ece99c276359869b4
Dana Milena Erro <danamerro@gmail.com>
2026-09-09 21:44:07 -0300

    finally y throw en Excepciones

--- Excepciones/ejemplo1/Form1.cs
+++ Excepciones/ejemplo1/Form1.cs
@@ -12,24 +12,45 @@ namespace ejemplo1
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
+            //int a, b, r;
+            int resultado;
+            
+            try
+            {
+                resultado = calcular();
+                /*
+                a = int.Parse(txt1.Text);
+                b = int.Parse(txt2.Text);
+                r = a + b;
+                */
+
+                lblResultado.Text = "= " + resultado;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error no conocido...");
+            }
+            finally 
+            { 
+                    //instrucciones
+                    //operacion sensible...
+            }
+        }
+
+        private int calcular() {
             int a, b, r;
             try
             {
                 a = int.Parse(txt1.Text);
                 b = int.Parse(txt2.Text);
                 r = a / b;
-                lblResultado.Text = "= " + r;
+                return r;
             }
-            catch (FormatException ex)
+            catch (Exception ex)
             {
-                MessageBox.Show("Por favor cargar solo numeros...");
-            }
-            catch (DivideByZeroException ex) {
-                MessageBox.Show("No se puede dividir por 0...");
-            }
-            
-            
 
+                throw ex;
+            }
         }
     }
 }
